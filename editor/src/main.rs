c7a332b8a1f9344c2be7df01ea893273e7e6e4ce
Mikhail Komarov <komarov.ma@phystech.edu>
2026-07-12 13:17:38 +0200

    Applied formatting

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
-use gart_editor::args::Args;
 use gart_editor::app::run;
+use gart_editor::args::Args;
 
 fn main() -> Result<()> {
     let args = Args::parse();
