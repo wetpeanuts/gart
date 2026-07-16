43d1ccb7a018ff411e83f74db845ff0175dbf911
Mikhail Komarov <komarov.ma@phystech.edu>
2026-07-16 21:41:42 +0200

    Added install script

---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+cargo install --path editor
+cargo install --path seq_editor
+
+mkdir -p $HOME/.local/bin
+
+ln -sf $HOME/.crate/bin/gart_editor $HOME/.local/bin/gart_editor
+ln -sf $HOME/.crate/bin/gart_seq_editor $HOME/.local/bin/gart_seq_editor
