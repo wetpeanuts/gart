22f4fb0bf01a0071726c2dacc297077d56c87be7
Mikhail Komarov <komarov.ma@phystech.edu>
2026-07-17 18:48:38 +0200

    Added --version option to gart_editor and gart_seq_editor

--- editor/src/args.rs
+++ editor/src/args.rs
@@ -3,6 +3,7 @@ use gart_common::args::parse_file_path;
 use std::path::PathBuf;
 
 #[derive(Parser)]
+#[command(version)]
 pub struct Args {
     #[arg(value_parser = parse_file_path)]
     pub file_path: PathBuf,
